8cd1b3a2da7437b0ded710f94e8ab7a98b51d728
badzim <113780781+badzim@users.noreply.github.com>
2026-05-07 11:07:15 +0200

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+        # Je dois construire un disctionnaire avec nombre de fois que j'ai vu un element
+        result = {}
+        for char in s:
+            result[char] = result.get(char, 0) + 1
+        for char in t:
+            result[char] = result.get(char, 0) - 1
+        # si la clé de l'un n'est pas dans l'autre c'est False
+        for char in result.keys():
+            if result[char] != 0:
+                return False
+        return True
